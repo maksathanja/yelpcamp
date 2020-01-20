11eff0c5cc647bfb5c2975fe41b8fed2ebc0ecff
Maksat Hanja <maxathanja@gmail.com>
2020-01-20 15:18:29 +0300

    Style with Bootstrap 3.4.1 (will be upgraded)

--- app.js
+++ app.js
@@ -22,6 +22,32 @@ const campgrounds = [
     name: "Mountain Goat's Rest",
     image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg',
   },
+  {
+    name: 'Salmon Creek',
+    image:
+      'https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069_960_720.jpg',
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_960_720.jpg',
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg',
+  },
+  {
+    name: 'Salmon Creek',
+    image:
+      'https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069_960_720.jpg',
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_960_720.jpg',
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg',
+  },
 ];
 
 app.get('/', (req, res) => {
