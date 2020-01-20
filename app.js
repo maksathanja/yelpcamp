7ab7c4e3e45637fb3e66fa2f6e2a7024ee6262f5
Maksat Hanja <maxathanja@gmail.com>
2020-01-20 15:54:48 +0300

    Add YelpCamp DB with Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -7,66 +8,59 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const database = `mongodb+srv://${username}:${password}@clusterone-36qke.mongodb.net/yelp_camp?retryWrites=true&w=majority`;
 
-const campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image:
-      'https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069_960_720.jpg',
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_960_720.jpg',
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg',
-  },
-  {
-    name: 'Salmon Creek',
-    image:
-      'https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069_960_720.jpg',
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_960_720.jpg',
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg',
-  },
-  {
-    name: 'Salmon Creek',
-    image:
-      'https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069_960_720.jpg',
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_960_720.jpg',
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg',
-  },
-];
+// Connect to database with mongoose
+mongoose.connect(database, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+// Campground Schema Setup
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+// Root route
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 // Campgrounds route
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', { campgrounds });
+  // Get all campgrounds from DB
+  Campground.find({}, (err, campgrounds) => {
+    if (err) {
+      console.log('Something happened:');
+      console.log(err);
+    } else {
+      res.render('campgrounds', { campgrounds });
+      // ? Also can be done like the following
+      // * Campground.find({}, (err, allCampgrounds) => { ...
+      // * res.render('campgrounds', campgrounds: allCampgrounds);
+    }
+  });
 });
 
 app.post('/campgrounds', (req, res) => {
   const { name } = req.body;
   const { image } = req.body;
   const newCampground = { name, image };
-  // get data from form and add to campgrounds array
-  campgrounds.push(newCampground);
-  // redirect back to /campgrounds page
-  res.redirect('/campgrounds');
+  // Create a new campground and save to DB
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to /campgrounds page
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
 app.get('/campgrounds/new', (req, res) => {
